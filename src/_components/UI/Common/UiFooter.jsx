09e35ff4d1b6ebf6ebdc4586d90b68960112b3df
malshan <malshan@ideahub.lk>
2019-08-12 18:53:32 +0800

    Footer component

--- src/_components/UI/Common/UiFooter.jsx
+++ src/_components/UI/Common/UiFooter.jsx
@@ -4,9 +4,18 @@ export const UiFooter = (props) => {
 
     return (
         <footer className="ui-footer">
-            <div className="ui-footer-inner">
-
-                <img src="../../../../styles/img/app_logo_dark.svg" className="ui-footer-logo"/>
+            <div className="ui-footer-inner clearfix">
+                <img src="../../../../styles/img/app_logo_dark.svg" className="ui-footer-logo f-left"/>
+                <div className="ui-footer-inner-left f-left">
+                    <p className="7">2019. All Rights Reserved. <a>Privacy Policy</a>   &nbsp;|&nbsp;   <a>Terms of Use</a></p>
+                </div>
+                <div className="ui-footer-inner-right">
+                    <a>About</a>
+                    <a>Help</a>
+                    <a>Contacts</a>
+                    <a>Partnerships</a>
+                    <a>FAQ</a>
+                </div>
             </div>
         </footer>
     );
