b21ccbc2079c56f708d5e75e91765a13b24cd33d
nelaka91 <nelaka@ideahub.lk>
2019-10-31 11:31:16 +0530

    item height

--- src/_components/UI/Common/UiSubNavMenuItem.jsx
+++ src/_components/UI/Common/UiSubNavMenuItem.jsx
@@ -8,30 +8,7 @@ import Icon from "@material-ui/core/Icon";
 import variables from '../../../../styles/_variables.scss';
 
 const useStyles = makeStyles({
-    plus: {
-        display: 'inline-block',
-        position: 'absolute',
-        top: 40,
-        right: 30,
-        cursor: 'pointer'    
-    },
-    title: {
-        fontSize: 16,
-        color: '#404553',
-        // fontFamily:Gelion,
-        fontWeight: 'bold',
-        lineHeight: 1.6,
-        marginBottom: 3,
-    },
-    desc: {
-        maxWidth: 140,
-        fontSize: 16,
-        color: '#404553',
-        // fontFamily:Gelion,
-        fontWeight: 'normal',
-        lineHeight: 1.06,
-        margin: 0,
-    },
+   
 });
 
 export const UiSubNavMenuItem = (props) => {
@@ -42,12 +19,12 @@ export const UiSubNavMenuItem = (props) => {
             <CardActionArea>
                 <CardContent className="navTileContent">
                     <h2 className="subNavCount">{props.count}</h2>
-                    <Icon className={classes.plus}>
+                    <Icon className="plus">
                         {props.iconUrl}</Icon>
-                    <Typography className={classes.title} component="h2">
+                    <Typography className="title" component="h2">
                         {props.tileTitle}
                     </Typography>
-                    <Typography className={classes.desc} component="p">
+                    <Typography className="desc" component="p">
                         {props.tileDesc}
                     </Typography>
                 </CardContent>
